9adbd8aa5649baf4e6a998104099ef331e224c08
davidbujok <davebujok@gmail.com>
2023-10-09 14:54:31 +0100

    Add correct streets without post codes

--- Bin_App_server/src/main/java/com/bin_app/scraper/Sanitize.java
+++ Bin_App_server/src/main/java/com/bin_app/scraper/Sanitize.java
@@ -12,6 +12,7 @@ public class Sanitize {
 
         HashMap<String, String> streets = new HashMap<>();
         ArrayList<String> correctStreets = new ArrayList<>();
+        ArrayList<String> notCorrectStreets = new ArrayList<>();
 
 
 		File fileS = new File("/Users/davidbujok/repos/Bin_App/Bin_App_server/src/main/java/com/bin_app/scraper/bin_pickup_days.tsv");
@@ -19,35 +20,22 @@ public class Sanitize {
 
         Scanner scanScrapper = new Scanner(fileS);
         Scanner scanScrapperCorrectStreets = new Scanner(fileStreetsWithCorrectNames);
-//        while (scanScrapper.hasNextLine()) {
-//            String data = scanScrapper.nextLine();
-//            String[] splitData = data.split("\t");
-//            streets.put(splitData[0], "s");
-//            System.out.println(splitData[0]);
-//        }
+        while (scanScrapper.hasNextLine()) {
+            String data = scanScrapper.nextLine();
+            String[] splitData = data.split("\t");
+            streets.put(splitData[0], splitData[2]);
+        }
         while (scanScrapperCorrectStreets.hasNextLine()) {
             String correctData = scanScrapperCorrectStreets.nextLine();
             String[] correctSplitData = correctData.split(" ");
             correctSplitData = Arrays.copyOf(correctSplitData, correctSplitData.length-2);
-//            correctSplitData = Arrays.copyOf(correctSplitData, correctSplitData.length-1);
             correctStreets.add(String.join(" ",correctSplitData));
         }
-        System.out.println(correctStreets);
 
         scanScrapper.close();
         scanScrapperCorrectStreets.close();
 
     }
 }
-//		try {
-//			File myObj = new File("filename.txt");
-//			Scanner myReader = new Scanner(myObj);
-//			while (myReader.hasNextLine()) {
-//				String data = myReader.nextLine();
-//				System.out.println(data);
-//			}
-//			myReader.close();
-//		} catch (FileNotFoundException e) {
-//			System.out.println("An error occurred.");
-//			e.printStackTrace();
-//		}
+
+
