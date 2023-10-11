831e87428d83cf23f34b0648e4606f6c42892eea
davidbujok <davebujok@gmail.com>
2023-10-11 16:01:00 +0100

    feat(repository): Street search, return limited to ten.

--- Bin_App_server/src/main/java/com/bin_app/repositories/StreetRepository.java
+++ Bin_App_server/src/main/java/com/bin_app/repositories/StreetRepository.java
@@ -1,9 +1,11 @@
 package com.bin_app.repositories;
 
 import com.bin_app.modules.Street;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -11,6 +13,13 @@ import java.util.List;
 @Repository
 public interface StreetRepository extends JpaRepository<Street, Long> {
 
+    List<Street> findByNameContainsAndCollectionDatesDateGreaterThan(String streetName, Date date, PageRequest of);
 
-    List<Street> findByCollectionDatesDateGreaterThan(Date date);
+    default List<Street> find10Streets(String streetName, Date date) {
+        return findByNameContainsAndCollectionDatesDateGreaterThan(streetName, date, PageRequest.of(0,20));
+    }
+
+
+    ;
+    List<Street> findByNameContains(String streetName);
 }
