4e5890f56edb6dc1b621773420d9c7a4d4f0dc4a
George Tegos <george.tegos.22@gmail.com>
2023-10-11 14:49:42 +0100

    query for collection after date

--- Bin_App_server/src/main/java/com/bin_app/repositories/StreetRepository.java
+++ Bin_App_server/src/main/java/com/bin_app/repositories/StreetRepository.java
@@ -4,7 +4,13 @@ import com.bin_app.modules.Street;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface StreetRepository extends JpaRepository<Street, Long> {
 
+
+    List<Street> findByCollectionDatesDateGreaterThan(Date date);
 }
