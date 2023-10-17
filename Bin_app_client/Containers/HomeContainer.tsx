0fb36437dbff7748b916c4771fd4ca0c56a27e6a
George Tegos <george.tegos.22@gmail.com>
2023-10-17 11:09:51 +0100

    containers+components

---
+++ Bin_app_client/Containers/HomeContainer.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Pressable, Text, TextInput, View } from 'react-native'
+import { styles,colourPalette } from '../styles/stylesSheet'
+
+
+function HomeContainer({heroText}) {
+  return (
+    <>
+            <View style={{alignSelf: 'center', paddingTop: 40}}>
+            <Text style={[heroText.hero, heroText.mid, colourPalette.green]}>Save</Text>
+            <Text style={[heroText.hero, heroText.light, colourPalette.green]}>the</Text>
+            <Text style={[heroText.hero, heroText.bold, colourPalette.green]}>Planet.</Text>
+            <Text style={[heroText.joinText, heroText.mid, colourPalette.blue]}>and</Text>
+            <View style={{flexDirection: 'row'}}>
+            <Text style={[heroText.letter, heroText.bold, colourPalette.red]}>R</Text>
+            <Text style={[ heroText.letter, heroText.bold, colourPalette.brown, ]}>e</Text>
+            <Text style={[ heroText.letter, heroText.bold, colourPalette.blue, ]}>c</Text>
+            <Text style={[ heroText.letter, heroText.bold, colourPalette.green, ]}>y</Text>
+            <Text style={[heroText.letter, heroText.bold, colourPalette.red]}>c</Text> 
+            <Text style={[ heroText.letter, heroText.bold, colourPalette.brown, ]}>l</Text>
+            <Text style={[ heroText.letter, heroText.bold, colourPalette.blue, ]}>e</Text>
+            </View>
+            </View>
+    </>
+  )
+}
+
+export default HomeContainer
