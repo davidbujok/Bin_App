29c2813b17e000f60eca9b84d66517a9ead6c1f2
George Tegos <george.tegos.22@gmail.com>
2023-10-18 19:01:56 +0100

    mock date/time , actualy date READY

--- Bin_app_client/Containers/SwipeableContainer.tsx
+++ Bin_app_client/Containers/SwipeableContainer.tsx
@@ -122,7 +122,7 @@ const Carousel = ({dates, streetName}) => {
 
   const handleNotification = async (date: IDate) =>{
     await checkApplicationPermission()
-    console.log(`${date.binType}`)
+    // console.log(`${date.binType}`)
     PushNotification.localNotification({
       channelId:"Date-Notification",
       title: "date.binType",
@@ -138,12 +138,25 @@ const Carousel = ({dates, streetName}) => {
     const month:number = Number(getMonth)
     const day:number = Number(getDay)
     console.log(typeof year, typeof month, typeof day)
+    console.log(`Year: ${year}, Month: ${month}, Day: ${day}`)
+
+
+    //Actual Variable (Need hour - minute variable)
+    // const setNotification = new Date(year,month-1,day,hour,minute)
+
+
+    // Presentation Date Time
+    // Need month - 1 because Date month starts from 0 , minutes shouldn't have 0 in front
+    const notificationDate = new Date(year,month-1,18,19,1)
+    console.log("Notification set for: ", notificationDate)
+   
 
     PushNotification.localNotificationSchedule({
       channelId: "Date-Notification",
       title: `Notification for ${date.date}`,
       message: `Your bin collection type for ${date.date} is ${date.binType}`,
-      date: new Date(Date.now() + 10 * 1000),
+      // date: new Date(Date.now() + 10 * 1000),
+      date:notificationDate,
       allowWhileIdle:true
     })
   }
