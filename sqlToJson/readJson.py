b3c81fb671db10fb4111511b711f797e2ecbf5a7
George Tegos <george.tegos.22@gmail.com>
2023-10-24 13:19:31 +0100

    Update converter, provide PDF links

--- sqlToJson/readJson.py
+++ sqlToJson/readJson.py
@@ -1,14 +1,35 @@
 import json
+import pandas as pd
+import numpy as np
+import pprint
 
 file = open('streets.json')
+cfile = pd.read_csv('calendar_links.tsv',sep="\t")
+
+cfile_id = cfile['calendar_id']
+cfile_link = cfile['calendar_download_link']
+
+print(cfile_id)
+
+calendar_dict={}
+
+i=0
+for id in cfile_id:
+    calendar_dict[id] = cfile_link[i]
+    i += 1
+
+
+# print(cfile)
+print(calendar_dict)
 
 data = json.load(file)
 
-print(type(data))
-print(data[0])
-
+#
+# print(type(data))
+# print(data[0])
+#
 new_format_data = []
-
+#
 for street in data:
     dict_name = street['name']
     dict_to_return = {}
@@ -16,9 +37,12 @@ for street in data:
         'garden_waste_id':street['garden_waste_id'],
         'garden_waste_url': street['garden_waste_url'],
         'postcode': street['postcode'],
-        'recycling_and_waste_id': street['recycling_and_waste_id'],
-        'recycling_and_waste_url': street['recycling_and_waste_url']
+        'recycling_and_waste_id': street['recycling_and_waste_id']
+#         # 'recycling_and_waste_url': street['recycling_and_waste_url']
     }
+    if street['recycling_and_waste_id'] in calendar_dict:
+        dict_to_return[dict_name]['recycling_and_waste_url'] = calendar_dict[street['recycling_and_waste_id']]
+
     new_format_data.append(dict_to_return)
 
 print(new_format_data[1])
