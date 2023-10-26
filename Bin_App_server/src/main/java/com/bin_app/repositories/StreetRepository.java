9ed6247ae8665270c9f06ffc1a7fa6d8bc044bbf
George Tegos <george.tegos.22@gmail.com>
2023-10-26 15:39:56 +0100

    controllers Test, get StreetName instead

--- Bin_App_server/src/main/java/com/bin_app/repositories/StreetRepository.java
+++ Bin_App_server/src/main/java/com/bin_app/repositories/StreetRepository.java
@@ -2,6 +2,8 @@ package com.bin_app.repositories;
 import com.bin_app.modules.Street;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.Date;
 import java.util.List;
@@ -15,9 +17,13 @@ public interface StreetRepository extends JpaRepository<Street, Long> {
         return findByNameContainsAndCollectionDatesDateGreaterThan(streetName, date, PageRequest.of(0,10));
     }
 
-    List<Street> findByNameContains(String streetName, PageRequest of);
+    @Query("SELECT s.name FROM Street s WHERE s.name LIKE %:streetName%")
+    List<String> findByNameContains(@Param("streetName")String streetName, PageRequest of);
 
-    default List<Street> findMax10Streets(String streetName) {
+    @Query("SELECT s.name FROM Street s")
+    List<String> findAllStreets();
+
+    default List<String> findMax10Streets(String streetName) {
         return findByNameContains(streetName, PageRequest.of(0,10));
     }
     ;}
